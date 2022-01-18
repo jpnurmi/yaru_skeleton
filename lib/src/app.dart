6769541c09e48f4d8b76e04e7bd2409fa2e502bd
J-P Nurmi <jp.nurmi@canonical.com>
2022-01-18 17:17:36 +0100

    Use Yaru theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:yaru/yaru.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
@@ -56,8 +57,8 @@ class MyApp extends StatelessWidget {
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
           // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
+          theme: yaruLight,
+          darkTheme: yaruDark,
           themeMode: settingsController.themeMode,
 
           // Define a function to handle named routes in order to support
